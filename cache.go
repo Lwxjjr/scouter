815fc0fc4ecc378a169d1999516ad05cd528a1ee
Lwx <2213196412@qq.com>
2026-03-23 22:05:31 +0800

    nil interface impl

--- cache.go
+++ cache.go
@@ -21,7 +21,7 @@ type Reader interface {
 
 type Result struct {
 	Code      uint16        // 状态码
-	Latency   time.Duration // 延迟
+	Latency   time.Duration // 延迟，指的是该次 HTTP 请求从发送到接收完成所花费的真实时间
 	Timestamp time.Time
 }
 
@@ -39,4 +39,12 @@ func NewCache(duration time.Duration) (Cache, error) {
 
 type cache struct {
 	storage tcore.Storage
+}
+
+func (c *cache) Insert(result *Result) error {
+	return nil
+}
+
+func (c *cache) Select(metric string, start, end time.Time) ([]float64, error) {
+	return nil, nil
 }
